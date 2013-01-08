b274f9a0cdb018913739b5a9edd2209a2a9805d1
Trevor Ewen <tewen@neosavvy.com>
2013-01-08 14:34:39 -0500

    Added the date time input.

---
+++ client/moe-app/src/main/resources/view/tickets/shared/date-time-input-directive.js
@@ -0,0 +1,26 @@
+'use strict'
+
+MOE.Directives
+    .directive('dateTimeInput', function () {
+        return {
+            restrict:'E',
+            replace:true,
+            template:'<div class="input"><input class="span2" type="text" ng-model="dateString"></div>',
+            scope:{
+                date: "=date",
+                format: "@format"
+            },
+            link:function (scope, element, attrs) {
+
+                scope.$watch('date', function(newValue) {
+                    if (newValue) {
+                        scope.dateString = newValue.strftime(scope.format);
+                    }
+                    return null;
+                });
+
+                //Initialization
+                scope.dateString = null;
+            },
+        }
+    });
