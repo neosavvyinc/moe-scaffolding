31690a92fc34db9a877909f4defa4f39395002c4
Trevor Ewen <tewen@neosavvy.com>
2013-01-08 12:06:44 -0500

    Added button dropdown but still having some small issues with the z-index.

--- client/moe-app/src/main/resources/view/tickets/equity/detail-ctrl.js
+++ client/moe-app/src/main/resources/view/tickets/equity/detail-ctrl.js
@@ -1,11 +1,14 @@
 'use strict'
 
-MOE.Controllers.controller('view.tickets.equity.DetailController', ['$scope', '$rootScope', '$routeParams', 'configuration', function ($scope, $rootScope, $routeParams, configuration) {
+MOE.Controllers.controller('view.tickets.equity.DetailController',
+    ['$scope', '$rootScope', '$routeParams', 'configuration',
+        function ($scope, $rootScope, $routeParams, configuration) {
 
     //Initialization
     $scope.timeInForceValues = ["Day", "Week", "Month"];
     $scope.yesNoValues = ["Yes", "No"];
     $scope.timeReceivedValues = ["Now", "Other"];
+    $scope.strategyValues = ["Execution Strategies", "Mediation Strategies", "Lunch Strategies"];
     $scope.dateValue = new Date();
 
 }]);
---
+++ client/moe-app/src/main/resources/view/tickets/shared/button-dropdown-directive.js
@@ -0,0 +1,31 @@
+'use strict'
+
+MOE.Directives
+    .directive('buttonDropdown', function () {
+        return {
+            restrict:'E',
+            replace:true,
+            templateUrl:"view/tickets/shared/button-dropdown-template.html",
+            scope:{
+                items: "=items"
+            },
+            link:function (scope, element, attrs) {
+                //Action Handlers
+                scope.onClickItem = function(item) {
+                    scope.selectedItem = item;
+                };
+
+                scope.$watch('items', function(newValue) {
+                    if (newValue && newValue.length) {
+                        scope.selectedItem = newValue[0];
+                    } else {
+                        scope.selectedItem = "No items";
+                    }
+                });
+
+                //Initialization
+                scope.selectedItem = null;
+                $(element).find('dropdown-toggle').dropdown();
+            },
+        }
+    });
