cc3458d4791c6c41c6f56ca694f159e5fb46551f
Trevor Ewen <tewen@neosavvy.com>
2013-01-14 18:18:54 -0500

    Still trouble.

--- moe-app/src/main/resources/application/view/worklists/tabs/tabs-ctrl.js
+++ moe-app/src/main/resources/application/view/worklists/tabs/tabs-ctrl.js
@@ -2,14 +2,6 @@
 
 MOE.Controllers.controller('views.worklists.tabs.TabsController', ['$scope', '$rootScope', '$routeParams', 'configuration', 'managers.LocalStorageManager', function ($scope, $rootScope, $routeParams, configuration, localStorageManager) {
 
-    //Get worklists
-    $scope.worklists = null;
-
-    //Event Listeners
-    $scope.$on(configuration.EVENTS.WORKLISTS_UPDATED, function(e, worklists) {
-       $scope.worklists = worklists;
-    });
-
     //Watchers
     $scope.$watch('selectedWorklist', function(newValue) {
         $rootScope.$broadcast(configuration.EVENTS.WORKLIST_SELECTED, newValue);
